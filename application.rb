31340404815274208dbcb12d992f5e753679def8
Nadia Barbosa <nadiabarbosa@me.com>
2016-03-13 12:31:58 -0400

    Adds completed Currency class

--- application.rb
+++ application.rb
@@ -2,26 +2,42 @@ require_relative './currency.rb'            # => true
 require_relative './currency_converter.rb'  # => true
 require 'pry'                               # => true
 
-currency = Currency.new("$1.00")  # => #<Currency:0x007fad29aafa68 @symbol="$", @value="1.00">
+currency = Currency.new("$1.00")  # => #<Currency:0x007fbca22c0950 @symbol="$", @value=1.0>
 
 code = currency.code     # => :USD
-amount = currency.value  # => "1.00"
+amount = currency.value  # => 1.0
 
 # WORKING CURRENCY CONVERSION!
-Convert.new(code, amount, :YEN)  # => #<Convert:0x007fad29aaf388 @code=:USD, @amount=1.0, @request=:YEN, @result=113.76564277588169>
+Convert.new(code, amount, :YEN)  # => #<Convert:0x007fbca22c0220 @code=:USD, @amount=1.0, @request=:YEN, @result=113.76564277588169>
 
 
 # ----- Currency Object Tests: -----
 # Equals another Currency object with the same amount and currency code
-Currency.new("$5.00") == Currency.new("$5.00")  # => false
+Currency.new("$5.00").is_equal?(Currency.new("$5.00"))  # => true
 
 # Does not equal another Currency object with different amount or currency code
 Currency.new("$5.00") == Currency.new("€5.00")  # => false
 
-c1 = Currency.new("$0.99")  # => #<Currency:0x007fad29aadfb0 @symbol="$", @value="0.99">
-c2 = Currency.new("$0.01")  # => #<Currency:0x007fad29aada10 @symbol="$", @value="0.01">
+c1 = Currency.new("$3.00")   # => #<Currency:0x007fbca22bb590 @symbol="$", @value=3.0>
+c2 = Currency.new("$2.00")   # => #<Currency:0x007fbca22baf28 @symbol="$", @value=2.0>
+c3 = Currency.new("€2.00")   # => #<Currency:0x007fbca22ba938 @symbol="€", @value=2.0>
+c4 = Currency.new("¥10.00")  # => #<Currency:0x007fbca22ba3e8 @symbol="¥", @value=10.0>
 
 # Like currencies can be added, regardless of value
-c1.add(c2)  # => #<Currency:0x007fad29aad4c0 @symbol="0", @value=".990.01">
+c1.add(c2)  # => 5.0
 
 # Like currencies can be subtracted, regardless of value
+c1.subtract(c2)  # => 1.0
+
+# Raise error if currencies can't be added or subtracted
+# c3.add(c4)       # ~> DifferentCurrencyCodeError: Can't add different currencies
+# c3.subtract(c4)
+
+# Multiply a currency by a fixed number or float, returning a new currency object
+c1.multiply(2)
+
+# ~> DifferentCurrencyCodeError
+# ~> Can't add different currencies
+# ~>
+# ~> /Users/nadiabarbosa/Repos/tiy-currency-converter/currency.rb:35:in `add'
+# ~> /Users/nadiabarbosa/Repos/tiy-currency-converter/application.rb:33:in `<main>'
--- currency.rb
+++ currency.rb
@@ -1,10 +1,11 @@
 class Currency
+
     def initialize(money_string)
         split_money_string = money_string.chars
         symbol = split_money_string.shift
         value = split_money_string.join
         @symbol = symbol
-        @value = value
+        @value = value.to_f
     end
 
     def code
@@ -20,11 +21,30 @@ class Currency
         @value
     end
 
+    def is_equal?(other)
+        @symbol==other.symbol && value == other.value
+    end
+
     def add(other)
         if @symbol == other.symbol
-            Currency.new(@value + other.value)
+            @value + other.value
         else
-            false
+            raise DifferentCurrencyCodeError, "Can't add different currencies"
         end
     end
+
+    def subtract(other)
+        if @symbol == other.symbol
+            @value - other.value
+        else
+            raise DifferentCurrencyCodeError, "Can't subtract different currencies"
+        end
+    end
+
+    def multiply(number)
+        Currency.new("#{@symbol}#{@value * number}")
+    end
+end
+
+class DifferentCurrencyCodeError < StandardError
 end
